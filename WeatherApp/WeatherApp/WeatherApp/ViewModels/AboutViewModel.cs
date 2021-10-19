ab4824c3d4b9df715947435a29c9ec4f5b7207b9
mateada966 <mateada966@student.polsl.pl>
2021-10-19 13:19:05 +0200

    Create AboutPage

--- WeatherApp/WeatherApp/WeatherApp/ViewModels/AboutViewModel.cs
+++ WeatherApp/WeatherApp/WeatherApp/ViewModels/AboutViewModel.cs
@@ -10,7 +10,7 @@ namespace WeatherApp.ViewModels
         public AboutViewModel()
         {
             Title = "About";
-            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://github.com/mateada966/WeatherApp-Xamarin"));
         }
 
         public ICommand OpenWebCommand { get; }
