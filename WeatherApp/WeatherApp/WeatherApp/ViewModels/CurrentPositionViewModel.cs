3e3ad5a69d72e27ba08fe0c16d19dc0d632ff55d
mateada966 <mateada966@student.polsl.pl>
2021-10-19 18:38:20 +0200

    Create CurrentPositionPage, Modify TabBar Content and Add CurrentPosition icon

---
+++ WeatherApp/WeatherApp/WeatherApp/ViewModels/CurrentPositionViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherApp.ViewModels
+{
+    class CurrentPositionViewModel : BaseViewModel
+    {
+        public CurrentPositionViewModel()
+        {
+            Title = "Current Position";
+        }
+    }
+}
