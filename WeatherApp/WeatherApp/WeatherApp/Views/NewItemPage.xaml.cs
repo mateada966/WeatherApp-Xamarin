8c3e741d38e5a45ed4eae58b26564f1df921f10b
mateada966 <mateada966@student.polsl.pl>
2021-10-19 12:18:53 +0200

    Add WeatherApp Xamarin project

---
+++ WeatherApp/WeatherApp/WeatherApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using WeatherApp.Models;
+using WeatherApp.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WeatherApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
