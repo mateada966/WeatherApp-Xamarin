73ea7a3e40b4e67066e643ad56201b690778f4d3
mateada966 <mateada966@student.polsl.pl>
2021-10-24 10:20:52 +0200

    Modify CurrentPositionPage. Add weather icons (wind, pressure, humidity, cloudiness). Add background logo.

--- WeatherApp/WeatherApp/WeatherApp/Views/CurrentPositionPage.xaml.cs
+++ WeatherApp/WeatherApp/WeatherApp/Views/CurrentPositionPage.xaml.cs
@@ -15,6 +15,35 @@ namespace WeatherApp.Views
         public CurrentPositionPage()
         {
             InitializeComponent();
+
+            //
+            this.BindingContext = this;
+            //
         }
+
+        //---
+        public List<Weather> Weathers { get => WeatherData(); }
+
+        private List<Weather> WeatherData()
+        {
+            var tempList = new List<Weather>();
+            tempList.Add(new Weather { Temp = "22", Date = "Sunday 16", Icon = "background_logo.png" });
+            tempList.Add(new Weather { Temp = "21", Date = "Monday 17", Icon = "background_logo.png" });
+            tempList.Add(new Weather { Temp = "20", Date = "Tuesday 18", Icon = "background_logo.png" });
+            tempList.Add(new Weather { Temp = "12", Date = "Wednesday 19", Icon = "background_logo.png" });
+            tempList.Add(new Weather { Temp = "17", Date = "Thursday 20", Icon = "background_logo.png" });
+
+            return tempList;
+        }
+        //---
     }
+
+    //---
+    public class Weather
+    {
+        public string Temp { get; set; }
+        public string Date { get; set; }
+        public string Icon { get; set; }
+    }
+    //---
 }
