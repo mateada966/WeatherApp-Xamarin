fae95743e8a6b991ad30948978bb36f49879c53d
mateada966 <mateada966@student.polsl.pl>
2021-10-24 15:21:09 +0200

    Add WeatherAPI.

---
+++ WeatherApp/WeatherApp/WeatherApp/Services/WeatherAPI/APICaller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Services.WeatherAPI
+{
+    public class APICaller
+    {
+        public static async Task<APIResponse> Get(string url, string authId = null)
+        {
+            using (var client = new HttpClient())
+            {
+                if (!string.IsNullOrWhiteSpace(authId))
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Authorization", authId);
+
+                var request = await client.GetAsync(url);
+                if (request.IsSuccessStatusCode)
+                {
+                    return new APIResponse { Response = await request.Content.ReadAsStringAsync() };
+                }
+                else
+                    return new APIResponse { ErrorMessage = request.ReasonPhrase };
+            }
+        }
+    }
+}
---
+++ WeatherApp/WeatherApp/WeatherApp/Services/WeatherAPI/APIResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherApp.Services.WeatherAPI
+{
+    public class APIResponse
+    {
+        public bool Successful => ErrorMessage == null;
+        public string ErrorMessage { get; set; }
+        public string Response { get; set; }
+    }
+}
